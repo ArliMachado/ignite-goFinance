2f9ed65046709ed67c79262add2580edbb43b818
Arli <arli.machado@gmail.com>
2021-07-28 17:01:44 -0300

    Definindo componente HighlightCard

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { HighLightCard } from '../../components/HighlightCard';
 
 
 import { 
   Container,
   Header,
+  HighLightCards,
   UserWrapper,
   UserInfo,
   Photo,
@@ -32,6 +34,30 @@ export function Dashboard() {
           
         </UserWrapper>
       </Header>
+
+      <HighLightCards>
+        <HighLightCard 
+          type="up"
+          title="Entradas" 
+          amount="R$ 17.400,00" 
+          lastTransaction="Última entrada dia 13 de abril"
+        />
+
+        <HighLightCard 
+          type="down"
+          title="Saídas" 
+          amount="R$ 1.259,00" 
+          lastTransaction="Última saída dia 03 de abril"
+        />
+
+        <HighLightCard 
+          type="total"
+          title="Total" 
+          amount="R$ 16.141,00" 
+          lastTransaction="01 à 16 de abril"
+        />
+
+      </HighLightCards>
     </Container>
   )
 }
