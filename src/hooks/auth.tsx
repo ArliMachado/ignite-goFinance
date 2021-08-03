a071b0885b4d3b6e713cf85e4ff2d3a41a8dc399
Arli <arli.machado@gmail.com>
2021-08-03 16:00:22 -0300

    definindo contexto de autenticacao

---
+++ src/hooks/auth.tsx
@@ -0,0 +1,43 @@
+import React,{ ReactNode, createContext, useContext } from "react";
+
+interface AuthProviderProps {
+  children: ReactNode
+}
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  photo?: string;
+}
+
+interface IAuthContextData {
+  user: User;
+}
+
+
+const AuthContext = createContext({} as IAuthContextData);
+
+
+function AuthProvider({ children }: AuthProviderProps) {
+
+  const user = {
+    id: '12312',
+    name: 'Arli',
+    email: 'arli@mail.com'
+  }
+  return (
+    <AuthContext.Provider value={{ user }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+function useAuth() {
+  const context = useContext(AuthContext);
+
+  return context;
+}
+
+
+export { AuthProvider, useAuth}
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -5,6 +5,7 @@ import AppleSvg from '../../assets/apple.svg';
 import GoogleSvg from '../../assets/google.svg';
 import LogoSvg from '../../assets/logo.svg';
 import { SignInSocialButton } from '../../components/SignInSocialButton';
+import { useAuth } from '../../hooks/auth';
 
 import {
   Container,
@@ -18,6 +19,10 @@ import {
 } from './styles';
 
 export function SignIn() {
+
+  const {user} = useAuth();
+  console.log(user);
+  
 
   return (
     <Container>
