f7d92a03623d9527202af69b0221e9f6a0bd361a
Arli <arli.machado@gmail.com>
2021-08-24 14:43:41 -0300

    iniciando configuração dos testes

---
+++ src/screens/Profile/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Button, Text, TextInput, View } from 'react-native';
+
+export function Profile() {
+  return (
+    <View>
+      <Text testID="text-title">Perfil</Text>
+
+      <TextInput
+        testID="input-name"
+        placeholder="Nome"
+        value="Arli"
+        autoCorrect={false}
+      />
+
+      <TextInput
+        testID="input-surname"
+        placeholder="Sobrenome"
+        value="Machado"
+      />
+
+      <Button title="Salvar" onPress={() => {}} />
+    </View>
+  );
+}
